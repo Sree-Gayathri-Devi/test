d9dfa7b1dbfa8034499dd2347c3d8000717cfb1b
unknown <gayathriganesan56@gmail.com>
2021-04-09 14:48:30 +0530

    added hungry prompt

--- hungry.py
+++ hungry.py
@@ -4,4 +4,8 @@ Created on Fri Apr  9 12:36:11 2021
 
 @author: GAYATHRI
 """
-print("i am hungry")
+hungry=input("are you hungry")
+if hungry=="yes":
+    print("eat samosa")
+else:
+    print("do your homework")
